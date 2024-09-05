d7f2286b1d78c7688fc82dd4e12996e19e469d0a
Ralf Geschke <ralf@kuerbis.org>
2024-09-05 12:21:33 +0200

    add previous/next navigation with Bootstrap cards´

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -9,43 +9,47 @@
       <main class="offset-md-1 offset-lg-1 col-md-7 ms-sm-auto col-lg-8 px-md-4">
         <Content class="vp-doc" />
 
-        <nav
-      v-if="control.prev?.link || control.next?.link"
-      class="prev-next"
-      aria-labelledby="doc-footer-aria-label"
-    >
-      <span class="visually-hidden" id="doc-footer-aria-label">Pager</span>
-
-      <div class="pager">
-        <a
-          v-if="control.prev?.link"
-          class="pager-link prev"
-          :href="control.prev.link"
-        >
-          <span
-            class="desc"
-            v-html="theme.docFooter?.prev || 'Previous page'"
-          ></span>
-          <span class="title" v-html="control.prev.text"></span>
-      </a>
-      </div>
-      <div class="pager">
-        <a
-          v-if="control.next?.link"
-          class="pager-link next"
-          :href="control.next.link"
-        >
-          <span
-            class="desc"
-            v-html="theme.docFooter?.next || 'Next page'"
-          ></span>
-          <span class="title" v-html="control.next.text"></span>
-      </a>
-      </div>
-    </nav>
 
 
-{{ control }}
+        <nav v-if="control.prev?.link || control.next?.link" class="prev-next" aria-labelledby="doc-footer-aria-label">
+          <span class="visually-hidden" id="doc-footer-aria-label">Pager</span>
+
+          <div class="row mt-4 mb-4">
+
+            <div v-if="control.prev?.link" class="col-sm-6 mb-3 mb-sm-0 pager">
+              <div class="card">
+                <div class="card-body">
+                  <span class="small" v-html="theme.docFooter?.prev || 'Previous page'"></span>
+                  <br />
+                  <a v-if="control.prev?.link" class="stretched-link pager-link prev" :href="control.prev.link">
+
+                    <span class="title" v-html="control.prev.text"></span>
+                  </a>
+
+
+                </div>
+              </div>
+            </div>
+            <div v-else class="col-sm-6 mb-3 mb-sm-0">
+            </div>
+            <div  v-if="control.next?.link" class="col-sm-6">
+              <div class="card text-end">
+                <div class="card-body">
+
+                  <span class="small" v-html="theme.docFooter?.next || 'Next page'"></span>
+                  <br />
+                  <a v-if="control.next?.link" class="stretched-link pager-link next" :href="control.next.link">
+
+                    <span class="title" v-html="control.next.text"></span>
+                  </a>
+
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </nav>
+
 
       </main>
       <div class="col-md-1 col-lg-1">&nbsp;</div>
